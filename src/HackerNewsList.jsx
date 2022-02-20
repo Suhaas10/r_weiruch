f7883c3cfe50c524f2f09d2ab84f79a898eb1dbf
Suhaas <suhaas69@gmail.com>
2022-02-20 05:30:10 +0530

    added dataFetchReducer

--- src/HackerNewsList.jsx
+++ src/HackerNewsList.jsx
@@ -1,47 +1,93 @@
 // `https://hn.algolia.com/api/v1/search?query=${input}`
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 
-const HackerNewsList = () => {
-  const [apiquery, setApiquery] = useState("");
-  const [url, setUrl] = useState(
-    "https://hn.algolia.com/api/v1/search?query=redux"
-  );
-  const [data, setData] = useState({ hits: [] });
-  const [isLoading, setisLoading] = useState(true);
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return { ...state, isLoading: true, isError: false };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case "FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+
+    default:
+      throw new Error();
+  }
+};
+
+const useDataApi = (initialUrl, initialData) => {
+  const [url, setUrl] = useState(initialUrl);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    data: initialData,
+    isLoading: false,
+    isError: false,
+  });
 
   useEffect(() => {
-    async function fetchData() {
-      setisLoading(true);
-      const response = await fetch(url);
-      const responseJson = await response.json();
+    dispatch({ type: "FETCH_INIT" });
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const responseJson = await response.json();
 
-      setData(responseJson);
-
-      setisLoading(false);
-    }
+        dispatch({ type: "FETCH_SUCCESS", payload: responseJson });
+      } catch (error) {
+        dispatch({ type: "FETCH_FAILURE" });
+      }
+    };
     fetchData();
   }, [url]);
+
+  return [state, setUrl];
+};
+
+const HackerNewsList = () => {
+  const [apiQuery, setApiQuery] = useState("");
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    "https://hn.algolia.com/api/v1/search?query=redux",
+    { hits: [] }
+  );
 
   return (
     <div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          setUrl(`https://hn.algolia.com/api/v1/search?query=${apiquery}`);
+          doFetch(`https://hn.algolia.com/api/v1/search?query=${apiQuery}`);
         }}
       >
         <input
           type="text"
-          value={apiquery}
-          onChange={(e) => setApiquery(e.target.value)}
+          value={apiQuery}
+          onChange={(e) => setApiQuery(e.target.value)}
+          placeholder="redux"
         />
         <button type="submit">Search</button>
       </form>
+
+      {isError}
+
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        data.hits.map((d) => <div>{d.title}</div>)
+        <ul>
+          {data.hits.map((d) => (
+            <li>
+              <a href={d.url}>{d.title}</a>
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   );
